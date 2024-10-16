c8861bbbfd5475b94876d1f43068be0dca21b68d
rayAritra <aritra.sohan@gmail.com>
2024-10-16 20:48:10 +0530

    1.project_setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async(req, res)=> {
+    res.json({
+        data: "you hit signup endpoint",
+    });
+}
+
+export const login = async(req, res)=> {
+    res.json({
+        data: "you hit signup endpoint",
+    });
+}
+
+export const logout = async(req, res)=> {
+    res.json({
+        data: "you hit signup endpoint",
+    });
+}
